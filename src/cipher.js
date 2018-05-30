649d7b668e91be7aa1331002e1781f70da41ed1f
MaryKattyVO <mkvicunaore@gmail.com>
2018-05-30 13:49:39 -0500

    cipher.js

--- src/cipher.js
+++ src/cipher.js
@@ -1,24 +1,51 @@
 //METODO CIFRAR
-const encode = () =>{
-    const keyNew = parseInt(key.value);
-    const messageC = msjInput.value;
-    var textOut = "";
-    for(let i = 0; i < messageC.length; i++){
-        var c = (messageC.charCodeAt(i)-65+keyNew)%26+65;
-        textOut = textOut + String.fromCharCode(c);
-    }
-    document.getElementById("msjOut").innerHTML=textOut;
+window.cipher = { 
+
+    //Metodo cifrar:    
+    encode: (offset , string) => {
+        const keyNew = parseInt(offset.value);
+        const msjToCipher = string.value;
+        let textOut = "";
+
+        for(let i = 0; i < msjToCipher.length; i++){
+            let convertToAsqui = msjToCipher.charCodeAt(i);
+
+            if(convertToAsqui >= 65 && convertToAsqui <= 90){
+                textOut = textOut + String.fromCharCode((convertToAsqui - 65+keyNew)%26 + 65);
+                console.log(textOut);
+            }
+            else if(convertToAsqui >= 97 && convertToAsqui <= 122){
+                textOut = textOut + String.fromCharCode((convertToAsqui - 97+keyNew)%26 + 97);
+            }
+       
+        }
+        return textOut;
+    },
+
+    //Metodo descifrar:
+    decode: (offset , string) => {
+        const keyNew = parseInt(offset.value);
+        const msjToCipher = string.value;
+        let textOut = "";
+
+        for(let i = 0; i < msjToCipher.length; i++){
+            let convertToAsqui = msjToCipher.charCodeAt(i);
+
+            if(convertToAsqui >= 65 && convertToAsqui <= 90){
+                textOut = textOut + String.fromCharCode((convertToAsqui - 65 - keyNew +  26*2)%26 + 65);
+                console.log(textOut);
+            }
+            else if(convertToAsqui >= 97 && convertToAsqui <= 122){
+                textOut = textOut + String.fromCharCode((convertToAsqui - 97 - keyNew +  26*2)%26 + 97);
+            }
+       
+        }
+        return textOut;
+    }   
+    
+
 
 }
-//METODO DECIFRAR
-const decode = () =>{
-    const keyNew = parseInt(key.value);
-    const messageC = msjInput.value;
-    var textOut = "";
-    for(let i = 0; i < messageC.length; i++){
-        var c = (messageC.charCodeAt(i)-65-keyNew+26*2)%26+65;
-        textOut = textOut + String.fromCharCode(c);
-    }
-    document.getElementById("msjOut").innerHTML=textOut;
-}
+
+
 
